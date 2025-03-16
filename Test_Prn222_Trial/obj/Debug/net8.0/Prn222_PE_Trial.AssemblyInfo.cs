4998fd691efae80d90b54deed05bf3a66633be5c
Tuan <kenjizukochannel@gmail.com>
2025-03-17 01:24:09 +0700

    1

--- Test_Prn222_Trial/obj/Debug/net8.0/Prn222_PE_Trial.AssemblyInfo.cs
+++ Test_Prn222_Trial/obj/Debug/net8.0/Prn222_PE_Trial.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prn222_PE_Trial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e88d8ab113c6fea890e44cecbe5ccd84eb18f9c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prn222_PE_Trial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prn222_PE_Trial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
